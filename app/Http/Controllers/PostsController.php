6f686a5d84604a22419c67da0bcdfb04c69aac8b
ramasd <ramunas.danupas@gmail.com>
2019-09-11 16:40:37 +0300

    Laravel From Scratch [Part 12] - File Uploading & Finishing Up

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Post;
 use DB;
 
@@ -58,14 +59,35 @@ class PostsController extends Controller
     {
         $this->validate($request, [
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'cover_image' => 'image|nullable|max:1999'
         ]);
+
+        // Handle File Upload
+        if($request->hasFile('cover_image')){
+            // Get file name with the extension
+            $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+
+            // Get just filenam
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            // Get just extension
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+
+            // Filename to store
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+
+            // Upload Image
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+        } else {
+            $fileNameToStore = 'noimage.jpg';
+        }
 
         // Create Post
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
         $post->user_id = auth()->user()->id;
+        $post->cover_image = $fileNameToStore;
         $post->save();
 
         return redirect('/posts')->with('success', 'Post Created');
@@ -112,7 +134,8 @@ class PostsController extends Controller
     {
         $this->validate($request, [
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'cover_image' => 'image|nullable|max:1999'
         ]);
 
         $post = Post::find($id);
@@ -121,10 +144,39 @@ class PostsController extends Controller
         if(auth()->user()->id !== $post->user_id){
             return redirect('/posts')->with('error', 'Unauthorized page');
         }
+
+        // Handle File Upload
+        if($request->hasFile('cover_image')){
+            // Get file name with the extension
+            $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+
+            // Get just filenam
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            
+            // Get just extension
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+
+            // Filename to store
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+
+            // Upload Image
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+        }
         
+        $old_image = $post->cover_image;
+
         // Update Post
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+
+        if($request->hasFile('cover_image')){
+            if($post->cover_image != 'noimage.jpg'){
+                // Delete Image
+                Storage::delete('public/cover_images/'.$post->cover_image);
+            }
+            $post->cover_image = $fileNameToStore;
+        }
+        
         $post->save();
 
         return redirect('/posts')->with('success', 'Post Updated');
@@ -143,6 +195,11 @@ class PostsController extends Controller
          // Check for correct user
          if(auth()->user()->id !== $post->user_id){
             return redirect('/posts')->with('error', 'Unauthorized page');
+        }
+
+        if($post->cover_image != 'noimage.jpg'){
+            // Delete Image
+            Storage::delete('public/cover_images/'.$post->cover_image);
         }
         $post->delete();
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,8 +6,15 @@
     @if(count($posts) > 0)
         @foreach($posts as $post)
             <div class='card p-3 mt-3 mb-3'>
-                <h3><a href='/posts/{{$post->id}}'>{{$post->title}}</a></h3> {{-- href="{{url('posts/'.$post->id)}}" --}} 
-                <small>Written on {{$post->created_at}} by <strong>{{$post->user->name}}</strong></small>
+                <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                    <img style="width:50%" src="/storage/cover_images/{{$post->cover_image}}">
+                    </div>
+                    <div class="col-md-8 col-sm-8">
+                        <h3><a href='/posts/{{$post->id}}'>{{$post->title}}</a></h3> {{-- href="{{url('posts/'.$post->id)}}" --}} 
+                        <small>Written on {{$post->created_at}} by <strong>{{$post->user->name}}</strong></small>
+                    </div>
+                </div>
             </div>
         @endforeach
         {{$posts->links()}}
